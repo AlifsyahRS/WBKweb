43ea9e0652e93a76de67990c95ba06fd372dace1
AlifsyahRS <alifsyah.sutarjadi@binus.ac.id>
2023-03-16 09:43:39 +0700

    Revised web pages

---
+++ modal_client.js
@@ -0,0 +1,30 @@
+var modal = document.getElementById("myModal");
+var header = document.getElementById("header");
+var background = document.getElementById("modal-background");
+
+const clientImg = [].slice.call(document.getElementsByClassName("clients-img"));
+
+
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+
+clientImg.forEach(img => {
+    img.onclick = function(){
+        if (this.id == "mandiri" || this.id == "sleman"){
+            background.style.backgroundColor = "#F0FFFF";
+        }
+        modal.style.display = "block";
+        header.style.display ="none";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+    }
+});
+
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function() {
+    modal.style.display = "none";
+    header.style.display = "block";
+    background.style.backgroundColor = "rgba(0, 0, 0, .0)";
+};
+
