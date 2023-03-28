3e1a8fdf248e60b7e016079a6019c2a29b19d05a
AlifsyahRS <alifsyah.sutarjadi@binus.ac.id>
2023-03-28 09:10:19 +0700

    Made the home, profile, contact, and product pages work in mobile

---
+++ header_dropdown.js
@@ -0,0 +1,14 @@
+
+var input = document.getElementById("more");
+var dropdown = document.getElementById("dropdown");  // 
+
+
+input.onclick = function(){
+    if (dropdown.style.display == "none"){
+        dropdown.style.display = "flex";
+        this.style.color = "#32E57A";
+    } else {
+        dropdown.style.display = "none";
+        this.style.color = "#FFFFFF";
+    }
+}
